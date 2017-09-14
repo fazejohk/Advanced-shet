26d5660067cdef526ee21872ff275df624e835fe
fj <johkmr@gmail.com>
2017-09-14 21:34:13 +0300

    turtle_shit

---
+++ turtle_shit/tt.py
@@ -0,0 +1,40 @@
+from turtle import *
+from random import randint
+
+speed(10)
+penup()
+goto(-140, 140)
+
+for step in range(11):
+    write(step, align="center")
+    right(90)
+    forward(10)
+    pendown()
+    forward(150)
+    penup()
+    backward(160)
+    left(90)
+    forward(20)
+
+ada = Turtle()
+ada.color('red')
+ada.shape('turtle')
+
+ada.penup()
+ada.goto(-160, 100)
+ada.pendown()
+
+bob = Turtle()
+bob.color('blue')
+bob.shape('turtle')
+
+bob.penup()
+bob.goto(-160, 70)
+bob.pendown()
+
+
+for turn in range(100):
+    ada.forward(randint(1,4))
+    ada.speed(randint(1,20))
+    bob.forward(randint(1,4))
+    bob.speed(randint(1,20))
